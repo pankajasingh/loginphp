bfa47d5eca5b346140e4eabab2645b8a54a19952
Pankaja Singh <singhpankaja2@gmail.com>
2018-08-09 22:51:13 +0530

    file with .php extension

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>foms</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+
+	<div class="login-box">
+		<img src="https://www.freeiconspng.com/uploads/profile-icon-person-user-19.png" class="profile">
+		<?php
+		echo"<h1>Login Form</h1>";
+		?>
+		<p>Username</p>
+		<input type="text" name="username" placeholder="Enter username here">
+		<p>Password</p>
+		<input type="password" name="pass" placeholder="Enter password here">
+		<button>login</button><br><br>
+		<a href="#">Forgot Password</a>
+	</div>
+
+
+</body>
+</html>
